ede3c8c18719b761d24f7629562fb4d794cfac05
Kieuanhdev <kieuanh.dev@gmail.com>
2025-11-11 22:19:39 +0700

    feat: Setup main.dart with Provider and blank home screen

---
+++ lib/features/tts_home/controller/tts_controller.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class TtsController with ChangeNotifier {
+  // TTS controller implementation
+}
---
+++ lib/features/tts_home/view/tts_home_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class TtsHomeScreen extends StatelessWidget {
+  const TtsHomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('TTS Pro - Day 1')),
+      body: const Center(child: Text('du an dang phat trien')),
+    );
+  }
+}
